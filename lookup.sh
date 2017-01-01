dadfc371acadcfe5c8157171f8b0867c8529f033
Joe Gallo <joe@brassafrax.com>
2016-12-31 21:31:15 -0500

    add info on zsh functions used

--- lookup.sh
+++ lookup.sh
@@ -1,9 +1,11 @@
 #!/usr/local/bin/zsh
 
+# used to produce table markdown
 rank=0
 for i in `cat data.json | jq -r '.[] | .host'`
 do
 ((rank++))
-local address=$(host2address $i)
+local address=$(host2address $i) # see zsh_functions.txt
 echo "| $rank | $i | $address |"
 done
+
